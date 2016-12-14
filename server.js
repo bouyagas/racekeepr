709a13715407c0f9716da8cfbc68da020cdb5547
Janelle Rosario <janelle.rosario@gmail.com>
2016-12-14 15:15:16 -0500

    package json edits for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express     = require('express');
 const logger      = require('morgan');
 const path        = require('path');
 const bodyParser  = require('body-parser');
-const raceRouter = require('./routes/api/races');
+const raceRouter  = require('./routes/api/races');
 
 const app         = express();
 const PORT        = process.argv[2] || process.env.PORT || 3000;
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // This will parse our payload from fetch which is sent as a JSON object
 app.use(bodyParser.json());
 
+// serve race routes
 app.use('/api/races', raceRouter);
 
 app.listen(PORT, () => console.log('server here! listening on', PORT));
